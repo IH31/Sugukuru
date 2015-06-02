97489dca0752606e324bf6d3d229e0db3f1c32aa
gituser <git@gituser.com>
2015-06-02 10:56:54 +0900

    add dao

---
+++ src/jp/ac/hal/tokyo/sugukuru/model/Dao.java
@@ -0,0 +1,45 @@
+package jp.ac.hal.tokyo.sugukuru.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 2015/06/01
+ * @author watanabe.yuta
+ */
+public abstract class Dao {
+	
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "";
+	private String user = "";
+	private String pass = "";
+	private Connection connection;
+		
+	
+	private Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(this.url, this.user, this.pass);
+	}
+	
+	/**
+	 * Connection Start
+	 */
+	public void open() {
+		// Class
+		try { Class.forName(this.driver); } catch(ClassNotFoundException e) { }
+		// Connection
+		try { this.connection = this.getConnection(); } catch(SQLException e) { }
+	}
+	
+	/**
+	 * Connection Close
+	 */
+	public void close() {
+		if(this.connection != null) {
+			try { this.connection.close(); } catch(SQLException e) { }
+		}
+	}
+}
