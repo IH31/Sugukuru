8f4cffc3a174d26d255dbfdff4b50c0a4b2e8423
Naventus <u.u.tau216@gmail.com>
2015-09-21 13:35:30 +0900

    Add file Validation

---
+++ src/sugukuru/validation/Validation.java
@@ -0,0 +1,14 @@
+package sugukuru.validation;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Validation {
+
+	private String[] ruleArr;
+	
+	public Validation(HttpServletRequest request, String[] ruleArr) {
+		
+	}
+}
