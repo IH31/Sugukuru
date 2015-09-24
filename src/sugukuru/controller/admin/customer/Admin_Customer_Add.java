2947ddefd433b31633d1f3c18c79dc02103d50da
Naventus <u.u.tau216@gmail.com>
2015-09-24 09:56:29 +0900

    Add Validation

--- src/sugukuru/controller/admin/customer/Admin_Customer_Add.java
+++ src/sugukuru/controller/admin/customer/Admin_Customer_Add.java
@@ -1,11 +1,15 @@
 package sugukuru.controller.admin.customer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import sugukuru.validation.Validation;
 
 /**
  * Servlet implementation class Admin_Customer_Add
@@ -36,15 +40,26 @@ public class Admin_Customer_Add extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		String customerName = request.getParameter("customerName");
-		String customerNameKana = request.getParameter("customerNameKana");
-		String departmentName = request.getParameter("departmentName");
-		String contactPersonName = request.getParameter("contactPersonName");
 		
-		System.out.println("customerName " + customerName);
-		System.out.println("customerNameKana " + customerNameKana);
-		System.out.println("departmentName " + departmentName);
-		System.out.println("contactPersonName " + contactPersonName);
+		
+		Validation valid =
+				new Validation(request,
+						new String[]{"customerName:number","email:email"});
+		
+		try {
+			if(valid.createValidation()) {
+				System.out.println("valid OK!");
+			} else {
+				System.out.println("valid NG...");
+			}
+			ArrayList<String> arr = valid.getErrCode();
+			for(String a : arr) {
+				System.out.println(a);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		
 		request.getRequestDispatcher("/WEB-INF/view/admin/customer/add.jsp").forward(request, response);
 	}
